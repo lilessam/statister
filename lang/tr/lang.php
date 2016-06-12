ea5f7d8294a01d8e0c3dd0e40cd61536e9817d63
Ahmed Essam <erwinchafros@gmail.com>
2016-06-12 20:49:45 +0200

    Adding Turkish Language

---
+++ lang/tr/lang.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'İstatistikler'
+        ],
+    'backend' => [
+        'title' => 'İstatistikler',
+        ],
+    'general'               => 'Genel İstatistikler',
+    'warning_title'         => 'Uyarı !',
+    'warning_fields'        => 'Sen öznitelikleri (col, label, color) belirtmek zorunda',
+    'warning_fields_end'    => ' YAML dosyasındaki alanlar',
+    'from'                  => 'Dan/Den ',
+    'unit'                  => 'birim: k',
+    'warning_table'         => 'Bir tablo belirtmek zorunda ve sınıf için niteliklerini ',
+    'warning_table_end'     => ' YAML dosyasındaki alanlar',
+    ];
